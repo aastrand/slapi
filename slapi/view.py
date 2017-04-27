50ab5da23ab9acbcacd1b646ec8c8204fd965842
Anders Åstrand <aastrand@gmail.com>
2017-04-27 20:34:28 +0200

    Fix typo

--- slapi/view.py
+++ slapi/view.py
@@ -3,7 +3,7 @@
 
 PRE_HEADER = ['<table id="sl_time_table">']
 PRE_HEADER.append('<tr><th style="width:3%"></th>')
-PRE_HEADER.append('<th style="width:75px;text-align:center"><img src="/static/SL_logo.svg" height="30px" /></th>'')
+PRE_HEADER.append('<th style="width:75px;text-align:center"><img src="/static/SL_logo.svg" height="30px" /></th>')
 HEADER_NAME = '<th style="color:#888888;padding-left:20;font-size:24px">%s </th>'
 POST_HEADER = [('<th style="color:#888888;width:12%;text-align:center;font-size:24px">min.</th>')]
 POST_HEADER.append('</tr>')
